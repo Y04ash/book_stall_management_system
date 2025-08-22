5a040db7a9e05f3a03cf7d0fc3348127a4f36722
Y04ash <yashranbhare2020@gmail.com>
2025-08-22 23:29:34 +0530

    route fix

--- client/src/Components/ProtectedRoute.jsx
+++ client/src/Components/ProtectedRoute.jsx
@@ -1,14 +1,15 @@
 // ProtectedRoute.js
 import React from 'react';
-import { Navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
 
 const ProtectedRoute = ({ children }) => {
+  const navigate = useNavigate();
   const token = Cookies.get('token');
   // console.log("all cookies ",Cookies.get())
   // console.log("doc cookies ",document.cookies)
   //   console.log("token from proteced route ",token)
-  return token ? children : <Navigate to="/unauth" />;
+  return token ? children : navigate("/unauth");
 };
 
 export default ProtectedRoute;
